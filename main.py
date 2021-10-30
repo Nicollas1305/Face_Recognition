8af4094f9e0c1f4f6536b42fc46ef6069813ecb2
Nicollas Marinho <cartola1320@gmail.com>
2021-10-30 15:42:34 -0300

    updates

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import cv2
 from simple_facerec import SimpleFacerec
 
-# Encode faces from a folder
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
 
-# Load Camera
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 
 while True:
     ret, frame = cap.read()
 
-    # Detect Faces
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
